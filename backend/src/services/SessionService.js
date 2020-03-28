1164f10303daa33d1b2e7d86cc88e69e27d92dfc
Mychell Teixeira <mychellt@gmail.com>
2020-03-28 00:14:51 -0300

    Cleaning the code

--- backend/src/services/SessionService.js
+++ backend/src/services/SessionService.js
@@ -9,6 +9,6 @@ module.exports = {
             return response.status(400).json({error: 'No ONG found with this ID'});
         }
 
-        return response.json(ong);
+        return response.json(ong); 
     }
 };
